58e61f9736377a9589cdda7f3a78e36e34a821c9
sharath kariyappa <sharathkariyappa98@gmail.com>
2025-06-19 21:17:43 +0530

    feat: update the model.

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app.main import app  # Import app object from main.py
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))  # Render will provide PORT env var
+    app.run(host='0.0.0.0', port=port)
